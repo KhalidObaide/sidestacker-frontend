2280f1cbc806f8c14cecf8cda56c79c222b1ee22
Khalid Obaide <sayedkhalidobaide@gmail.com>
2022-11-15 18:42:18 +0300

    Implement and integrate lobby

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   GameStatus,
   ReadyStatuses,
   Direction,
-  StatusMessages,
+  StatusMessages
 } from "./structures";
 
 const SOCKET_SERVER = "http://54.235.114.223:5000";
@@ -15,6 +15,14 @@ const socket = io(SOCKET_SERVER);
 
 function App() {
   const boardRef = useRef<any>(null);
+  const [fullname, setFullname] = React.useState<string>("");
+  const [gameCode, setGameCode] = React.useState<number>(0);
+  const [error, setError] = React.useState<string>("");
+
+  React.useEffect(() => {
+      if ( error ) alert(error)
+  }, [error]);
+
   const [game, setGame] = React.useState<Game>({
     status: GameStatus.Disconnected,
     game_id: 0,
@@ -24,13 +32,18 @@ function App() {
 
   const matchGame = () => {
     socket.connect();
-    socket.emit("match");
+    socket.emit("match", { fullname, code: gameCode });
     if (boardRef.current) boardRef.current.resetMasterBoard();
   };
   const leaveGame = () => socket.emit("leave");
   const makeMove = (row: number, direction: Direction) => {
     socket.emit("move", { move: direction.toString() + row.toString() });
   };
+
+  socket.on("error", (error: string) => {
+      socket.disconnect();
+      setError(error);
+  });
 
   socket.on("game", (game: Game) => {
     setGame(game);
@@ -41,7 +54,23 @@ function App() {
 
   return (
     <div className="App">
-      <div>{StatusMessages[game.status]}</div>
+      <div>{StatusMessages[game.status]} | {game.game_id} </div>
+      <div>
+        <input
+            type="text"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            className="input-basic"
+            placeholder="Fullname"
+        />
+        <input
+            type="number"
+            value={gameCode}
+            onChange={(e) => setGameCode(parseInt(e.target.value))}
+            className="input-basic"
+            placeholder="Game Code"
+        />
+      </div>
       {ReadyStatuses.includes(game.status) && (
         <button className="cta" onClick={matchGame}>
           Join Game
